e6b2baeef2cc3acedc1b768b619b9c162e9f419a
Abraham Chen <abes@Abrahams-MacBook-Pro.local>
2017-11-07 22:51:47 -0800

    Actually solved this thing exponentially

--- exponential_solution.py
+++ exponential_solution.py
@@ -6,16 +6,19 @@ import itertools
 def wizards(numWiz, numCon, array_Wiz, arrayCon):
 	allCombos = itertools.permutations(array_Wiz,len(array_Wiz))
 	for item in allCombos:
-		if fulfils_all_constraints(item):
-			print(item)
+		print("Item is: ","".join(item))
+		if fulfils_all_constraints("".join(item),arrayCon):
+			print("VALID: ",item)
 	return "DONE"
 
 
-def fulfils_all_constraints(constraints,ordering):
+def fulfils_all_constraints(ordering,constraints):
 	return all([fulfils(constraint,ordering) for constraint in constraints])
 
 
 def fulfils(constraint,ordering):
+	print("Constraint is: ",constraint)
+	print("Ordering is: ",ordering)
 	dependency = ordering.index(constraint[-1])
 	first_val = ordering.index(constraint[0])
 	second_val = ordering.index(constraint[1])
