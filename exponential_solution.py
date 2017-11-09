a0fe35a4a74792523637747cd0560565034be49c
Jared Gutierrez <j.gutierrez1@berkeley.edu>
2017-11-09 13:24:03 -0800

    trial

--- exponential_solution.py
+++ exponential_solution.py
@@ -7,16 +7,17 @@ def parser(filename):
 	constraint_list = [] 
 	for element in k[3:]:
 		constraint_list.append(element.replace(" ","").replace("\n",""))
-	return (wizards,constraint_list)
+	return (list(wizards),constraint_list)
 
 
 
 def wizards(array_Wiz, arrayCon):
 	allCombos = itertools.permutations(array_Wiz,len(array_Wiz))
 	for item in allCombos:
-		print("Item is: ","".join(item))
+		# print("Item is: ","".join(item))
 		if fulfils_all_constraints("".join(item),arrayCon):
-			print("VALID: ",item)
+			joinedItem = "".join(item)
+			print("VALID: ",joinedItem)
 	return "DONE"
 
 
@@ -25,9 +26,12 @@ def fulfils_all_constraints(ordering,constraints):
 
 
 def fulfils(constraint,ordering):
-	print("Constraint is: ",constraint)
-	print("Ordering is: ",ordering)
+	# print("Constraint is: ",constraint)
+	# print("Ordering is: ",ordering)
 	dependency = ordering.index(constraint[-1])
 	first_val = ordering.index(constraint[0])
 	second_val = ordering.index(constraint[1])
 	return dependency < first_val or dependency > second_val
+
+
+
